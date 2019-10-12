1ba0c8df982061d1e31311da0bad0fd7ad08e0fc
Lynn Mitchell <lynn7mitchell@gmail.com>
2019-10-11 20:17:47 -0500

    created button component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
+import Button from '../components/Button'
 
 export class Home extends Component {
     render() {
 
         const style={
-            textAlign: "center"
+            textAlign: "center",
+            marginTop: "25vh"
         }
 
         return (
             <div style={style}>
                 <h1>Japanese Writing</h1>
                 <h2>Learn Japanese Katakana, Hiragana, and Kanji</h2>
+                <Button content={"Log In"}/> <Button content={"Sign Up"}/>
             </div>
         )
     }
