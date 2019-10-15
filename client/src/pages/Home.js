42ba969138a51d391e44c1b30fcc273621898f5d
Lynn Mitchell <lynn7mitchell@gmail.com>
2019-10-14 22:29:40 -0500

    deleted button component for css reasons

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,22 +1,28 @@
-import React, { Component } from 'react'
-import Button from '../components/Button'
+import React, { Component } from "react";
 
 export class Home extends Component {
-    render() {
+  render() {
+    const style = {
+      main: {
+        textAlign: "center",
+        marginTop: "25vh"
+      },
+      button:{
+          marginLeft: "10%",
+          marginRight: "10%",
+          marginTop: "10%"
+      }
+    };
 
-        const style={
-            textAlign: "center",
-            marginTop: "25vh"
-        }
-
-        return (
-            <div style={style}>
-                <h1>Japanese Writing</h1>
-                <h2>Learn Japanese Katakana, Hiragana, and Kanji</h2>
-                <Button content={"Log In"}/> <Button content={"Sign Up"}/>
-            </div>
-        )
-    }
+    return (
+      <div style={style.main}>
+        <h1>Japanese Writing</h1>
+        <h2>Learn Japanese Katakana, Hiragana, and Kanji</h2>
+        <button style={style.button}>Log In</button>
+        <button style={style.button}> Sign Up</button>
+      </div>
+    );
+  }
 }
 
-export default Home
+export default Home;
