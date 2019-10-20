5453709ccd217203f8df3587a0fbe28c96624736
Lynn Mitchell <lynn7mitchell@gmail.com>
2019-10-20 10:07:17 -0500

    onClick checks answer in multiple choice

--- client/src/pages/MultipleChoiceKana.js
+++ client/src/pages/MultipleChoiceKana.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import setAuthToken from "../utils/setAuthtoken";
+import axios from "axios";
 import katakana from "../katakana.json";
-import hiragana from "../hiragana.json"
+import hiragana from "../hiragana.json";
 
 export class MultipleChoiceKana extends Component {
   state = {
@@ -8,12 +10,30 @@ export class MultipleChoiceKana extends Component {
     languageArray: [],
     answer: {},
     choices: [],
-    listedAnswers: []
+    listedAnswers: [],
+    currentStreak: 0,
+    highestStreak: 0
   };
 
   
 
   componentWillMount() {
+    const token = localStorage.getItem("example-app");
+
+    if (token) {
+      setAuthToken(token);
+    }
+
+    axios
+      .get("api/user")
+      .then(response => {
+        this.setState({
+          user: response.data,
+          highestStreak: response.data.katakana.multipleChoice.highest
+        });
+      })
+      .catch(err => console.log(err.response));
+
     if (this.state.language === "katakana") {
       let languageArray = katakana;
       let answer =
@@ -113,6 +133,17 @@ export class MultipleChoiceKana extends Component {
       });    }
   }
 
+
+  onClick = e =>{
+    let userAnswer = e.target.innerHTML
+
+    if(userAnswer === this.state.answer.roumaji){
+      console.log("true")
+    }else if(userAnswer !== this.state.roumaji){
+      console.log("false")
+    }
+  }
+
   
   render() {
     const style = {
@@ -123,22 +154,26 @@ export class MultipleChoiceKana extends Component {
     };
     return (
       <div style={style.main}>
+         <div className="streak">
+          <h3>Highest Streak: {this.state.highestStreak}</h3>
+          <h3>Current Streak: {this.state.currentStreak}</h3>
+        </div>
         <h1>{this.state.answer.kana}</h1>
         <div className="container">
           <div className="row">
-            <div className="col s4 container-outline" name={this.state.listedAnswers[0].roumaji}>
+            <div className="col s4 container-outline" name={this.state.listedAnswers[0].roumaji} onClick={this.onClick}>
                 {console.log(this.state.listedAnswers)}
               {this.state.listedAnswers[0].roumaji}
             </div>
-            <div className="col s4 container-outline" name={this.state.listedAnswers[1].roumaji}>
+            <div className="col s4 container-outline" name={this.state.listedAnswers[1].roumaji} onClick={this.onClick}>
               {this.state.listedAnswers[1].roumaji}
             </div>
           </div>
           <div className="row">
-            <div className="col s4 container-outline" name={this.state.listedAnswers[2].roumaji}>
+            <div className="col s4 container-outline" name={this.state.listedAnswers[2].roumaji} onClick={this.onClick}>
               {this.state.listedAnswers[2].roumaji}
             </div>
-            <div className="col s4 container-outline" name={this.state.listedAnswers[3].roumaji}>
+            <div className="col s4 container-outline" name={this.state.listedAnswers[3].roumaji} onClick={this.onClick}>
               {this.state.listedAnswers[3].roumaji}
             </div>
           </div>
