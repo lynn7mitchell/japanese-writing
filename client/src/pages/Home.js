28c4944a9524e4006730a351b3a3677b61f0487c
lynn7mitchell <lynn7mitchell@gmail.com>
2021-05-06 22:36:44 -0500

    fixed desktop home design

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,22 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 export default function Home() {
   return (
-    <div className="bg-gray-900 h-screen w-screen flex flex-col items-center content-center justify-center  font-bold font-m-plus-rounded text-white">
+    <div className="bg-gray-900 h-screen w-screen flex flex-col items-center content-center justify-center font-bold font-m-plus-rounded text-white">
       <h1 className="lg:text-6xl text-3xl font-bold font-m-plus-rounded pb-1">
         Japanese Writing App
       </h1>
-      <h3 className="text-lg text-gray-600 font-m-plus-rounded" >Learn Japanese Hiragana &amp; Katakana</h3>
-      <div className="flex flex-col lg:flex-row content-around justify-self-end mt-96">
+      <h3 className="text-lg text-gray-600 font-m-plus-rounded">
+        Learn Japanese Hiragana &amp; Katakana
+      </h3>
+      <div className="flex flex-col lg:flex-row content-around justify-self-end lg:mt-80 mt-96">
         <Link to={"/login"} className="pb-5">
-          <button className="bg-blue-700 text-lg rounded-lg p-2 text-white w-screen font-bold font-m-plus-rounded">
+          <button className="bg-blue-700 lg:w-40 lg:mx-36 text-lg rounded-lg p-2 text-white w-screen font-bold font-m-plus-rounded">
             LOG IN
           </button>
         </Link>
         <Link to={"/signup"}>
-          <button className="bg-blue-700 text-lg rounded-lg p-2 text-white w-screen font-bold font-m-plus-rounded">
+          <button className="bg-blue-700 lg:w-40 lg:mx-36 text-lg rounded-lg p-2 text-white w-screen font-bold font-m-plus-rounded">
             SIGN UP
           </button>
         </Link>
