8b2621fec90ce31ecf50803d67514f7422453c46
Lynn Mitchell <lynn7mitchell@gmail.com>
2021-07-17 21:42:10 -0500

    added indication of a demo login on the home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import arrow_icon from "../arrow_icon.png";
 
 export default function Home() {
   return (
@@ -11,15 +12,38 @@ export default function Home() {
         Learn Japanese Hiragana &amp; Katakana
       </h3>
       <div className="flex flex-col lg:flex-row content-around justify-self-end lg:mt-80 mt-96">
+        
         <Link to={"/login"} className="pb-5">
+        <div className="w-full flex justify-center -mb-10 relative">
+          <h3 className="self-top " >
+            Demo Login Available
+          </h3>
+          <svg
+            id="Layer_1"
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 23.18 37.92"
+            width="4%"
+            className="self-bottom pl-2"
+            style={{ fill: "#fefefe" }}
+          >
+            <path
+              d="M180.9,150.4c.72-3,1.9-5.92,2.07-8.92.36-6.83-2.76-11.93-8.81-15.19a9.47,9.47,0,0,1-1.78-1.1,1.93,1.93,0,0,1-.47-2.72,2.21,2.21,0,0,1,2.92-1q11.74,5.33,12.83,18.19c.36,4.45-.94,8.53-3.15,12.89l5-2c.64-.25,1.27-.54,1.91-.8a2.27,2.27,0,0,1,3.08,1.16,2.49,2.49,0,0,1-1.26,3.25q-6,2.46-11.94,4.82a2.49,2.49,0,0,1-3.26-1.5c-1.6-3.88-3.19-7.76-4.74-11.65a2.37,2.37,0,0,1,1.33-3.33c1.15-.45,2.36.22,3,1.62l2.62,6.21Z"
+              transform="translate(-171.54 -121.22)"
+            />
+          </svg>
+        </div>
+        <div className="pt-12">
           <button className="bg-blue-700 lg:w-40 lg:mx-36 text-lg rounded-lg p-2 text-white w-screen font-bold font-m-plus-rounded">
             LOG IN
-          </button>
+          </button></div>
         </Link>
         <Link to={"/signup"}>
+        <div className="lg:pt-12">
+
           <button className="bg-blue-700 lg:w-40 lg:mx-36 text-lg rounded-lg p-2 text-white w-screen font-bold font-m-plus-rounded">
             SIGN UP
-          </button>
+          </button></div>
         </Link>
       </div>
       {/* <p>Demo Login:</p>
